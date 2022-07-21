60108b0ea86b2e05e233f37ebd8d7087fabf208b
m4rcer 4 <m4rcer@gmail.com>
2022-07-21 22:40:45 +0300

    Готово

---
+++ app.js
@@ -0,0 +1,60 @@
+$(function() {
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop(); ;
+
+    checkScroll(scrollPos, introH);
+    
+    $(window).on("scroll load resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop(); 
+        checkScroll(scrollPos, introH);        
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if(scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+
+    
+    let nav = $("#nav");
+    
+    $("#navToggle").on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+
+
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+      });
+})
